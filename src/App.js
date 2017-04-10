7598130a6221f589a1788f8c005752bca0a62ce2
Juan Manuel Rodriguez <jmrodriguez@medallia.com>
2017-04-10 11:44:08 -0700

    react-native-web added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { WebButton, WebButtonColor  } from '../packages/web-elements'
+import { ActivityIndicator } from 'react-native';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header" style={{backgroundColor:WebButtonColor}}>
           <img src={logo} className="App-logo" alt="logo" />
+          <ActivityIndicator size='large' color='white'/>
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
